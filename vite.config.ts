82f3e1f32d5596b446a662982b134b733a2569dc
Eyerusalem Moges Kumsa <jerusalemmoges1111@gmail.com>
2025-07-31 01:37:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/foodie-finder/", // <-- Add this line
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
